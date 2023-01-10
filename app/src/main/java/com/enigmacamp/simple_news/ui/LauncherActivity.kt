a374b99c7d6cd615192900bacd25dab06775d56d
edwardsuwirya <edward_suwirya@yahoo.com>
2023-01-10 15:27:25 +0700

    Change splash screen to activity

---
+++ app/src/main/java/com/enigmacamp/simple_news/ui/LauncherActivity.kt
@@ -0,0 +1,20 @@
+package com.enigmacamp.simple_news.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.enigmacamp.simple_news.R
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1500)
+    }
+}
--- app/src/main/java/com/enigmacamp/simple_news/ui/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/simple_news/ui/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.enigmacamp.simple_news.ui
 
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.enigmacamp.simple_news.R
 import com.enigmacamp.simple_news.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
-import java.util.*
-import kotlin.concurrent.schedule
 
 
 class MainActivity : DaggerAppCompatActivity() {
@@ -17,12 +14,7 @@ class MainActivity : DaggerAppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashScreen = installSplashScreen()
-        splashScreen.setKeepOnScreenCondition { true }
         super.onCreate(savedInstanceState)
-        Timer().schedule(3000) {
-            splashScreen.setKeepOnScreenCondition { false }
-        }
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment =
